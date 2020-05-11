00beb06160397620807055982667bef7058f85b4
DonViktorash <vik.markin2014@yandex.ru>
2020-05-11 22:25:57 +0300

    "Wait for string connections. Add DatabaseHelper"

--- app/src/main/java/com/example/dvor/SmartDvorDatabaseHelper.java
+++ app/src/main/java/com/example/dvor/SmartDvorDatabaseHelper.java
@@ -4,16 +4,14 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 import com.example.smartdvor.R;
-import com.example.smartdvor.RegisterFragment;
 
 //Тестовый класс. Бд создается внутри и здесь же она удаляется после работы.
 
 public class SmartDvorDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "dvor"; //Database name
-    private static final int DB_VERSION = 2 ;     //Database version
+    private static final int DB_VERSION = 1 ;     //Database version
 
     SmartDvorDatabaseHelper(Context context){
         super(context, DB_NAME, null, DB_VERSION);
@@ -45,21 +43,37 @@ public class SmartDvorDatabaseHelper extends SQLiteOpenHelper {
          *Все входные данные являются шаблонными. Необходимо разработать примерное видение и менять параметры
          *Дедлайн на определение базы данных - пятница 1 мая.
     */
-    private static void insertProfilesData(SQLiteDatabase db, String name, String description, int resourceId){
+//    private static void insertProfilesData(SQLiteDatabase db, String name, String description, int resourceId){
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put("NAME", name); //.put кладет в определенное поле данные. Надо таким образом заполнить все поля строки
+//        contentValues.put("DESCRIPTION", description);
+//        contentValues.put("IMAGE_RESOURCE_ID", resourceId);
+//        db.insert("DRINK", null, contentValues);
+//    }
+    public boolean insertClientsData(SQLiteDatabase db, String phoneNumber, String password, String street, String houseNumber, String apartNumber) {
         ContentValues contentValues = new ContentValues();
-        contentValues.put("NAME", name); //.put кладет в определенное поле данные. Надо таким образом заполнить все поля строки
-        contentValues.put("DESCRIPTION", description);
-        contentValues.put("IMAGE_RESOURCE_ID", resourceId);
-        db.insert("DRINK", null, contentValues);
+        contentValues.put("phoneNumber", phoneNumber); //.put кладет в определенное поле данные. Надо таким образом заполнить все поля строки
+        contentValues.put("password", password);
+        contentValues.put("street", street);
+        contentValues.put("houseNumber", houseNumber);
+        contentValues.put("apartNumber", apartNumber);
+        long result = db.insert("clients", null, contentValues); //кладем в таблицу наши поля.
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
     }
-
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion){
         if(oldVersion < 1){
-            db.execSQL("CREATE TABLE DRINK(_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                    " NAME TEXT, " +
-                    "DESCRIPTION TEXT, " +
-                    "IMAGE_RESOURCE_ID INTEGER);");
-            insertProfilesData(db, "Latte", "Espresso and steamed milk", R.drawable.background_splash);
+            db.execSQL("CREATE TABLE CLIENTS(_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " PHONENUMBER TEXT, " +
+                    "PASSWORD TEXT, " +
+                    "STREET TEXT,"+
+                    "HOUSENUMBER TEXT, "+
+                    "APARTNUMBER TEXT);");
+//            insertProfilesData(db, "Latte", "Espresso and steamed milk", R.drawable.background_splash);
+            insertClientsData(db, "+71234567890", "User111", "Pushlinskaya","1", "1");
         }
         if(oldVersion < 2){
             /*
@@ -72,9 +86,25 @@ public class SmartDvorDatabaseHelper extends SQLiteOpenHelper {
             //NUMERIC -> добавление числового столбца
         }
     }
+    public void getAllDataAboutUser(){
 
+    }
 }
-
+/*
+Андрей. Это тебе надо вставить в код, откуда ты будешь брать б.д.
+SmartDvorDatabaseHelper smartDvorDatabaseHelper = new SmartDvorDatabaseHelper(this);
+//getReadableDatabase() -> только для чтения
+//getWritableDatabase() -> для выполнения обновления данных
+//SQLiteDatabase db = SmartDvorDatabaseHelper.getReadableDatabase();
+//SQLiteDatabase db = SmartDvorDatabaseHelper.getWritableDatabase();
+try{
+    SQLiteDatabase db = SmartDvorDatabaseHelper.getReadableDatabase();
+    //Код чтения данных из базы
+    } catch(SQLiteException){
+    Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+    toast.show(); //в этой строке уведомление выводится на экран
+}
+ */
 /*
 *   Для обновления данных необходимо заново создать ContentValues
 *   и положить в него то поле, которое необходимо обновить, а далее применить метод update()
